c4e02546a41e9a71a730c59adaea9d4e5468a58e
josearmandojacq <josearmandocastilloquiala@Joses-MacBook-Pro.fritz.box>
2018-01-09 17:55:43 +0100

    Updated the bar chart with svg

--- Bar Chart with SVG/index.js
+++ Bar Chart with SVG/index.js
@@ -1,40 +1,50 @@
-let data = [4,8,15,16,23,42];
+let data = [42, 23, 16, 13, 10, 8];
 
 let width = 420,
-    barHeight= 20;
-
+    barHeight = 20;
+//Scaling
 let x = d3.scaleLinear()
     .domain([0, d3.max(data)])
-    .range([0, width])
+    .range([0, width]);
 
-let chart = d3.select('.chart')
-            .attr('width', width)
-            .attr('height', barHeight * data.length);
+let y = d3.scaleLinear()
+    .range([barHeight, 0])
+
+//Adding the axis
+let xAxis = d3.axisBottom()
+    .scale(x)
+
+let yAxis = d3.axisLeft()
+    .scale(y)
+        
+
+let chart = d3.select(".chart")
+    .attr("width", width)
+    .attr("height", barHeight * data.length);
+    
+chart.append('g')
+    .attr('transform', 'translate(0,50)')
+    .attr('class', 'x axis')
+    .call(xAxis);
+
+let bar = chart.selectAll(".barG")
+    .data(data)
+    .enter().append("g")
+    .attr('class', 'barG')
+    .attr("transform", (d, i) => { return "translate(0," + i * barHeight + ")"; });
 
 
-let bar = chart.selectAll('g')
-            .data(data)
-                .enter()
-                .append('g')
-                .attr('transform', (d,i)=>{
-                    return `translate(0, ${i + barHeight})`;
-                })
-                .attr('padding', '10')
+bar.append('g')
+    .attr('class', 'y axis')
+    .call(yAxis)
+    
 
-bar.append('rect')
-    .attr('width', x)
-    .attr('height', barHeight - 1)
-    .attr('fill', 'steelblue');
+bar.append("rect")
+    .attr("width", x)
+    .attr("height", barHeight - 1);
 
-bar.append('text')
-    .attr('x', (d)=>{
-        return x(d) - 3;
-    })
-    .attr('y', barHeight / 2)
-    .attr('dy', '.35em')
-    .attr('fill', 'white')
-    .attr('font', '10px sans-serif')
-    .attr('text-anchor', 'end')
-    .text((d)=>{
-        return d;
-    });
+bar.append("text")
+    .attr("x", (d) => { return x(d) - 3; })
+    .attr("y", barHeight / 2)
+    .attr("dy", ".35em")
+    .text((d) => { return d; });
